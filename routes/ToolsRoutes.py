bcbc7b2935edcb67b0258e02475dd343e24b6648
AnubhavBharadwaaj <anubhav27071997@gmail.com>
2023-04-15 22:46:49 +0530

    removed dummy statement

--- routes/ToolsRoutes.py
+++ routes/ToolsRoutes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Path, Query, Request
 from .services.services import *
 
 router = APIRouter()
-print("Change")
+
 @router.get("/video/{url:path}")
 async def get_video(url):
     return {"url": youtube_video_downloader(url)[1:-1]}
